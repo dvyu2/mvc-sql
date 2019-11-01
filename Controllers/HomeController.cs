fb1e7417e17466cbc46d097196f8936df5632a1a
Dvin <dvyu@yandex.ru>
2019-11-01 19:30:51 +0500

    first init mvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc_sql.Models;
+
+namespace mvc_sql.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Help(string name)
+        {
+            return "Hi this is my SQL server, supported methods: Get,Add,Del";
+        }
+
+        public string Get(string name)
+        {
+            return "Get";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
